b2e4682f06d466dd8682cfaa0f82a626d7e9d320
ralphbartley <ralphgbartley@gmail.com>
2021-03-23 18:03:13 +0000

    added rspec test database and helper
    Co-authored-by: Natalia <77491121+TataSher@users.noreply.github.com>

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,5 +1,5 @@
 feature 'user arrives at home page' do
-  scenario 'it should welcome the user' do
+  scenario 'it should welcome the user' do  
     visit '/'
     expect(page).to have_content("Welcome to the Bookmark Manager!")
   end
--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -5,6 +5,10 @@ describe Bookmark do
 
   describe '#self.all' do
     it 'lists all stored bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+
       expect(Bookmark.all).to include("http://www.makersacademy.com")
     end
   end
